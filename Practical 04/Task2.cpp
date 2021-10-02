9eb7b1c05f1c7652ed761c75d66b6a1af9ecb438
Malkeet Singh <90818931+Malkeet-Chahal@users.noreply.github.com>
2021-10-02 19:07:43 +0530

    Add files via upload

---
+++ Practical 04/Task2.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+using namespace std;
+namespace first{
+	int add(int a, int b)
+	{
+		return (a+b);
+	}
+}
+namespace second{
+	float add(float a, float b)
+	{
+		return (a+b);
+	}
+}
+
+using namespace first;
+using namespace second;
+
+int main()
+{
+	cout << add(2,3) << endl;
+	cout << add(2.2f,3.3f) << endl;
+	cout << add(0,0)<< endl;
+	cout << add(5.5f,4.1f)<< endl;
+	return 0;
+}
+	
+	
+	
+	
+	
+	
+	
