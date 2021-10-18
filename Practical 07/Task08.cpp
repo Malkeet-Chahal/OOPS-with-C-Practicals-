00d63f1669c8da8069a37b85c71c4c4d8625b9b7
Malkeet Singh <90818931+Malkeet-Chahal@users.noreply.github.com>
2021-10-18 09:11:24 +0530

    Add files via upload

---
+++ Practical 07/Task08.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    char arr[4][4];
+    for(int i = 0; i < 4; i++)
+    {
+        for(int j = 0; j < 4; j++)
+        if(i + j <= 3)
+        cout << "*";
+        else 
+        cout << " ";
+        cout << endl;
+    }
+    return 0;
+}
