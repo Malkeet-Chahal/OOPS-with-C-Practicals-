a38425e9f2c04bb6e475beaf2fbfbb96c9470d2a
Malkeet Singh <90818931+Malkeet-Chahal@users.noreply.github.com>
2021-11-20 11:57:40 +0530

    Add files via upload

---
+++ Practical 10/Task04_V2.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+class overload
+{
+    private:
+        int a;
+    public:
+        overload(int a)
+        {
+            this->a = a;
+        }
+        friend int operator+(overload &s,overload &s2);
+};
+int operator+(overload &s,overload &s2)
+{
+    return(s.a+s2.a);
+}
+int main()
+{
+    int a,b;
+    cout<<"Enter the value of a and b simultaneously :"<<endl;
+    cin>>a>>b;
+    overload a1(a),a2(b);
+    //operator overloading to plus the objects
+    cout<<"Sum = "<<a1+a2<<endl;
+    return 0;
+}
